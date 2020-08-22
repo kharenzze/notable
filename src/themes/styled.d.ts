f017915548bac03776f532b7e69125c3e1d70871
Paolo <str237@gmail.com>
2020-08-23 01:49:51 +0200

    Apply prettier

--- src/themes/styled.d.ts
+++ src/themes/styled.d.ts
@@ -3,30 +3,30 @@ import 'styled-components'
 
 // and extend them!
 declare module 'styled-components' {
-    export interface DefaultTheme {
-        colors: {
-            themePrimary: string
-            themeLighterAlt: string
-            themeLighter: string
-            themeLight:  string
-            themeTertiary: string
-            themeSecondary: string
-            themeDarkAlt: string
-            themeDark: string
-            themeDarker: string
-            neutralLighterAlt: string
-            neutralLighter: string
-            neutralLight: string
-            neutralQuaternaryAlt: string
-            neutralQuaternary: string
-            neutralTertiaryAlt: string
-            neutralTertiary: string
-            neutralSecondary: string
-            neutralPrimaryAlt: string
-            neutralPrimary: string
-            neutralDark: string
-            black: string
-            white: string
-        }
+  export interface DefaultTheme {
+    colors: {
+      themePrimary: string
+      themeLighterAlt: string
+      themeLighter: string
+      themeLight: string
+      themeTertiary: string
+      themeSecondary: string
+      themeDarkAlt: string
+      themeDark: string
+      themeDarker: string
+      neutralLighterAlt: string
+      neutralLighter: string
+      neutralLight: string
+      neutralQuaternaryAlt: string
+      neutralQuaternary: string
+      neutralTertiaryAlt: string
+      neutralTertiary: string
+      neutralSecondary: string
+      neutralPrimaryAlt: string
+      neutralPrimary: string
+      neutralDark: string
+      black: string
+      white: string
     }
+  }
 }
