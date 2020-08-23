f23fda2c23f9774c2c2acf54c85d4f835a32f348
Paolo <str237@gmail.com>
2020-08-24 01:06:28 +0200

    Custom togglew

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import ReactToggle from 'react-toggle'
+import 'react-toggle/style.css'
+
+export const Toggle = styled(ReactToggle).attrs({
+  icons: false,
+})`
+  &.react-toggle--checked .react-toggle-track {
+    background: ${(props) => props.theme.colors.neutralLight};
+  }
+  &.react-toggle--checked:hover .react-toggle-track {
+    background: ${(props) => props.theme.colors.neutralLighter};
+  }
+  &.react-toggle .react-toggle-track {
+    background: ${(props) => props.theme.colors.neutralLight};
+  }
+  &.react-toggle:hover .react-toggle-track {
+    background: ${(props) => props.theme.colors.neutralLighter};
+  }
+`
