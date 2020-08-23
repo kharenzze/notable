92321b85764c754e5b865f875767861a00b0a818
Paolo <str237@gmail.com>
2020-08-24 01:26:53 +0200

    Render toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Container, TopBar } from "./layout";
 import { ThemeManager } from "./themes/ThemeManager";
+import { ThemeToogle } from "./components/ThemeToggle";
 
 function App() {
   return (
     <ThemeManager>
       <Container>
         <TopBar>
-
+          <ThemeToogle/>
         </TopBar>
       </Container>
     </ThemeManager>
