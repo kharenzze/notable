87c9f8579cec5b9eafa330067a13c0667f696a02
Paolo <str237@gmail.com>
2020-08-23 15:44:20 +0200

    Add theme context provider

--- src/themes/ThemeManager.tsx
+++ src/themes/ThemeManager.tsx
@@ -2,7 +2,12 @@ import React, { FC } from 'react'
 import { Dark } from './themes'
 import { EmptyProps } from '../common'
 import { ThemeProvider } from 'styled-components'
+import { ThemeContext } from './Context'
 
 export const ThemeManager: FC<EmptyProps> = ({ children }) => {
-  return <ThemeProvider theme={Dark}>{children}</ThemeProvider>
+  return (
+    <ThemeContext.Provider value={{}}>
+      <ThemeProvider theme={Dark}>{children}</ThemeProvider>
+    </ThemeContext.Provider>
+  )
 }
