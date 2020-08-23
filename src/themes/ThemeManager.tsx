632236b5daaa43cd2d7193b4f5298c75f8a17805
Paolo <str237@gmail.com>
2020-08-23 21:11:39 +0200

    So that we can toogle themes

--- src/themes/ThemeManager.tsx
+++ src/themes/ThemeManager.tsx
@@ -1,13 +1,31 @@
-import React, { FC } from 'react'
-import { Dark } from './themes'
+import React, { FC, useMemo, useState } from 'react'
+import { DefaultTheme } from 'styled-components'
+import { Dark, Light } from './themes'
 import { EmptyProps } from '../common'
 import { ThemeProvider } from 'styled-components'
-import { ThemeContext } from './Context'
+import { ThemeContext, ThemeContextProps } from './Context'
+
+const useTheme = () => {
+  const [theme, setTheme] = useState<DefaultTheme>(Dark)
+  const themeContextValue: ThemeContextProps = useMemo<ThemeContextProps>(() => {
+    return {
+      toogleTheme: () => {
+        setTheme((t) => (t === Dark ? Light : Dark))
+      },
+    }
+  }, [setTheme])
+
+  return {
+    theme,
+    themeContextValue,
+  }
+}
 
 export const ThemeManager: FC<EmptyProps> = ({ children }) => {
+  const { theme, themeContextValue } = useTheme()
   return (
-    <ThemeContext.Provider value={{}}>
-      <ThemeProvider theme={Dark}>{children}</ThemeProvider>
+    <ThemeContext.Provider value={themeContextValue}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </ThemeContext.Provider>
   )
 }
