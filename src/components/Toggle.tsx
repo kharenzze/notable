8e346b842176205f1884d4e56d354f8524f49458
Paolo <str237@gmail.com>
2020-08-24 01:33:43 +0200

    Adjust colors

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -6,15 +6,15 @@ export const Toggle = styled(ReactToggle).attrs({
   icons: false,
 })`
   &.react-toggle--checked .react-toggle-track {
-    background: ${(props) => props.theme.colors.neutralLight};
+    background: ${(props) => props.theme.colors.neutralLighter};
   }
   &.react-toggle--checked:hover .react-toggle-track {
-    background: ${(props) => props.theme.colors.neutralLighter};
-  }
-  &.react-toggle .react-toggle-track {
     background: ${(props) => props.theme.colors.neutralLight};
   }
-  &.react-toggle:hover .react-toggle-track {
+  &.react-toggle .react-toggle-track {
     background: ${(props) => props.theme.colors.neutralLighter};
+  }
+  &.react-toggle:hover .react-toggle-track {
+    background: ${(props) => props.theme.colors.neutralLight};
   }
 `
