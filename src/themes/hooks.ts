dd42a9fb3758764abf7bd10569b9cf79a9c37eac
Paolo <str237@gmail.com>
2020-08-24 01:11:13 +0200

    Wrap hooks

--- src/themes/hooks.ts
+++ src/themes/hooks.ts
@@ -2,5 +2,5 @@ import { ThemeContext } from 'styled-components'
 import { useContext } from 'react'
 import { SwitchThemeContext } from './Context'
 
-export const useSwitchThemeContext = useContext(SwitchThemeContext)
-export const useTheme = useContext(ThemeContext)
+export const useSwitchThemeContext = () => useContext(SwitchThemeContext)
+export const useTheme = () => useContext(ThemeContext)
