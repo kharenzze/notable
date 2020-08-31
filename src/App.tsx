9600d6fffcf3d2d5d2c8e44aa724142d90ddea2c
Paolo <str237@gmail.com>
2020-08-31 21:53:27 +0200

    Use debounced save

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React, { useMemo, useCallback } from 'react'
-import { MainContainer, TopBar } from "./layout";
-import { ThemeManager } from "./themes/ThemeManager";
-import { ThemeToogle } from "./components/ThemeToggle";
-import { NotesContainer } from "./layout";
-import { Slate, Editable, withReact } from "slate-react";
-import { useAppState } from "./hooks/storageHooks";
-import { createEditor } from "slate";
+import { MainContainer, TopBar } from './layout'
+import { ThemeManager } from './themes/ThemeManager'
+import { ThemeToogle } from './components/ThemeToggle'
+import { NotesContainer } from './layout'
+import { Slate, Editable, withReact } from 'slate-react'
+import { useAppState, useSaveOnLocalStorage } from './hooks/storageHooks'
+import { createEditor } from 'slate'
 
 function App() {
   const editor = useMemo(() => withReact(createEditor()), [])
   const [value, setValue] = useAppState()
-  const onChange = useCallback((next) => setValue(next), [setValue])
+  const save = useSaveOnLocalStorage()
+  const onChange = useCallback(
+    (next) => {
+      setValue(next)
+      save(next)
+    },
+    [setValue, save]
+  )
   return (
     <ThemeManager>
       <MainContainer>
         <TopBar>
-          <ThemeToogle/>
+          <ThemeToogle />
         </TopBar>
         <NotesContainer>
           <Slate editor={editor} value={value} onChange={onChange}>
