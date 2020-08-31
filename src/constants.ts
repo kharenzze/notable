9ccd1be90450d136f88ab10dd50e922090b7f8b1
Paolo <str237@gmail.com>
2020-08-31 22:23:58 +0200

    Define constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export enum STORAGE_KEY {
+  STATE = 'STATE',
+  OPTIONS = 'OPTIONS',
+}
+
+export const DEBOUNCED_SAVE_WAIT = 3000
--- src/hooks/storageHooks.ts
+++ src/hooks/storageHooks.ts
@@ -2,11 +2,10 @@ import store from 'store2'
 import { useState, useCallback } from 'react'
 import { Node } from 'slate'
 import debounce from 'lodash/debounce'
-
-const STORAGE_KEY = 'STATE'
+import { DEBOUNCED_SAVE_WAIT, STORAGE_KEY } from '../constants'
 
 const getStateFromStorage: () => Node[] = () => {
-  const state = store.get(STORAGE_KEY)
+  const state = store.get(STORAGE_KEY.STATE)
   if (state) {
     return JSON.parse(state)
   }
@@ -22,14 +21,12 @@ export const useAppState = () => {
   return useState<Node[]>(getStateFromStorage)
 }
 
-const DEBOUNCE_WAIT = 3000
-
 export const useSaveOnLocalStorage = () => {
   return useCallback(
     debounce((state: Node[]) => {
       const stateString = JSON.stringify(state)
-      store.set(STORAGE_KEY, stateString)
-    }, DEBOUNCE_WAIT),
+      store.set(STORAGE_KEY.STATE, stateString)
+    }, DEBOUNCED_SAVE_WAIT),
     []
   )
 }
