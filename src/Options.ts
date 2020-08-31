95b167072ff295fc774a895c19de327ba4e7a88c
Paolo <str237@gmail.com>
2020-08-31 23:46:07 +0200

    Added options manager

---
+++ src/Options.ts
@@ -0,0 +1,43 @@
+import store from 'store2'
+import { STORAGE_KEY } from './constants'
+
+enum ThemeType {
+  Dark = 'dark',
+  Light = 'light',
+}
+
+interface IOptions {
+  theme: ThemeType
+}
+
+const defaultOptions: IOptions = {
+  theme: ThemeType.Light,
+}
+
+class OptionsCls {
+  private readonly state: IOptions
+  constructor() {
+    const storedData = store.get(STORAGE_KEY.OPTIONS)
+    if (storedData) {
+      this.state = JSON.parse(storedData)
+    } else {
+      this.state = defaultOptions
+    }
+  }
+
+  public getTheme() {
+    return this.state.theme
+  }
+
+  public setTheme(theme: ThemeType) {
+    this.state.theme = theme
+    this.save()
+  }
+
+  private save() {
+    const asString = JSON.stringify(this.state)
+    store.set(STORAGE_KEY.OPTIONS, asString)
+  }
+}
+
+export const Options = new OptionsCls()
