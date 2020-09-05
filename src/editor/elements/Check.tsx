a2256583dd7876639f5f16567d5baf20b3c8de6f
Paolo <str237@gmail.com>
2020-09-05 20:28:28 +0200

    Add container

--- src/editor/elements/Check.tsx
+++ src/editor/elements/Check.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
 const CheckText = styled.p`
   color: ${(p) => p.theme.colors.black};
 `
 
 export const Check: FC = ({ children, ...rest }) => {
-  return <CheckText>{children}</CheckText>
+  return (
+    <Container>
+      <CheckText>{children}</CheckText>
+    </Container>
+  )
 }
