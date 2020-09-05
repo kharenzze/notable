ab6d854e596d89c3902b623f8d203cbaa105bea3
Paolo <str237@gmail.com>
2020-09-05 21:53:14 +0200

    Copntainer should not be contentEditable. Import Checkbox

--- src/editor/elements/Check.tsx
+++ src/editor/elements/Check.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
+import { Checkbox } from '../../components/Checkbox'
 
-const Container = styled.div`
+const Container = styled.div.attrs({
+  contentEditable: false,
+})`
   display: flex;
   flex-direction: row;
 `
 
-const CheckText = styled.p`
+const CheckText = styled.span`
   color: ${(p) => p.theme.colors.black};
 `
 
 export const Check: FC = ({ children, ...rest }) => {
   return (
     <Container>
+      <Checkbox checked={true} />
       <CheckText>{children}</CheckText>
     </Container>
   )
