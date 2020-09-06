a70a774b17dbe42ef60b84074c8c04e54cb7482c
Paolo <str237@gmail.com>
2020-09-06 12:00:43 +0200

    Checkbox: display check

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -7,17 +7,45 @@ interface ICheckbox {
   className?: string
 }
 
-interface ThemedFn extends ThemedStyledProps<ICheckbox, DefaultTheme> {}
+interface IContainer {
+  onClick?: () => void
+  className?: string
+}
 
-const getColor = (p: ThemedFn) =>
-  p.checked ? p.theme.colors.themePrimary : p.theme.colors.themeDarker
+interface IThemedContainer extends ThemedStyledProps<IContainer, DefaultTheme> {}
+
+const getColor = (p: IThemedContainer) => p.theme.colors.themeDarker
+const getBorderColor = (p: IThemedContainer) => p.theme.colors.themePrimary
 
 const Container = styled.div`
   height: 14px;
   width: 14px;
-  background: ${getColor};
+  background: transparent;
+  border: 2px solid ${getBorderColor};
+  position: relative;
+  color: ${getColor};
+`
+
+interface ICheckMark {
+  checked: boolean
+}
+
+interface IThemedCheckmark extends ThemedStyledProps<ICheckMark, DefaultTheme> {}
+
+const getDisplayCheckmark = (p: IThemedCheckmark) => (p.checked ? 'block' : 'none')
+
+const CheckMark = styled.span`
+  display: ${getDisplayCheckmark};
+  position: absolute;
+  top: -12px;
+  font-weight: bolder;
+  font-size: 26px;
 `
 
 export const Checkbox: FC<ICheckbox> = ({ checked, onClick, className = '' }) => {
-  return <Container checked={checked} onClick={onClick} className={className} />
+  return (
+    <Container onClick={onClick} className={className}>
+      <CheckMark checked={checked}>&#x2713;</CheckMark>
+    </Container>
+  )
 }
