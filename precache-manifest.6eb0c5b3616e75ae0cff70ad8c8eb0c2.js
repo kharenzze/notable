a825fde041038793f24be26baf8bd30ba9291c22
kharenzze <kharenzze@users.noreply.github.com>
2020-09-29 09:36:09 +0000

    deploy: 5e8bf3758287c05a83b5651f0a1d6c217b45c8a1

---
+++ precache-manifest.6eb0c5b3616e75ae0cff70ad8c8eb0c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da29c456d2457bee359e3325efc08967",
+    "url": "/notable/index.html"
+  },
+  {
+    "revision": "17eb94c5533e797395ea",
+    "url": "/notable/static/css/2.9de2e04a.chunk.css"
+  },
+  {
+    "revision": "724050013cb7c59b56a0",
+    "url": "/notable/static/css/main.43c8d928.chunk.css"
+  },
+  {
+    "revision": "17eb94c5533e797395ea",
+    "url": "/notable/static/js/2.62c64677.chunk.js"
+  },
+  {
+    "revision": "a87affafbb0ec03c23b068c3a11f4965",
+    "url": "/notable/static/js/2.62c64677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724050013cb7c59b56a0",
+    "url": "/notable/static/js/main.0c5f6679.chunk.js"
+  },
+  {
+    "revision": "bc7faf35596c32937264",
+    "url": "/notable/static/js/runtime-main.4308bcc3.js"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/notable/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "a1375023bb3f9d55dfc6d58685cd6e69",
+    "url": "/notable/static/media/NotoSans-BoldItalic.a1375023.ttf"
+  },
+  {
+    "revision": "afe1714f485b424f221147e1be6cf8ad",
+    "url": "/notable/static/media/NotoSans-Italic.afe1714f.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/notable/static/media/NotoSans-Regular.5a87cce8.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f091fda331adc14d521b52c23b49482d.js"
+  "/notable/precache-manifest.6eb0c5b3616e75ae0cff70ad8c8eb0c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/notable/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
