e2f6b558353cfe8bbe2fab1d0ccba2fe3949ef5a
kharenzze <kharenzze@users.noreply.github.com>
2020-09-29 09:17:17 +0000

    deploy: 9785347c777b87366db0eff2b41eb1be5ae50a7b

---
+++ precache-manifest.f091fda331adc14d521b52c23b49482d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71eddf3c9b618d901297a25482c847b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f06194771db5735a6d",
+    "url": "/static/css/2.9de2e04a.chunk.css"
+  },
+  {
+    "revision": "3333fb5f87173886a89c",
+    "url": "/static/css/main.850ea9c0.chunk.css"
+  },
+  {
+    "revision": "e1f06194771db5735a6d",
+    "url": "/static/js/2.3ea13e48.chunk.js"
+  },
+  {
+    "revision": "a87affafbb0ec03c23b068c3a11f4965",
+    "url": "/static/js/2.3ea13e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3333fb5f87173886a89c",
+    "url": "/static/js/main.a687fc43.chunk.js"
+  },
+  {
+    "revision": "93f12337479fadd9d298",
+    "url": "/static/js/runtime-main.1d3721a2.js"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "a1375023bb3f9d55dfc6d58685cd6e69",
+    "url": "/static/media/NotoSans-BoldItalic.a1375023.ttf"
+  },
+  {
+    "revision": "afe1714f485b424f221147e1be6cf8ad",
+    "url": "/static/media/NotoSans-Italic.afe1714f.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/static/media/NotoSans-Regular.5a87cce8.ttf"
+  }
+]);
